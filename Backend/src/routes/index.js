60e1ea337683e76d7ad98739cf054ac57a247471
Q-UBAIDULLAH <qaziubaidullah5@gmail.com>
2025-08-18 13:38:44 -0700

    created

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import UserRoutes from '../modules/user/routes.js'
 import productroutes from '../modules/product/route.js'
+import Cartroutes from '../modules/cart/route.js'
 import uploadroute from'../modules/upload/route.js'
+
 const router=Router()
 
 router.use('/user',UserRoutes)
 router.use('/product',productroutes)
+router.use('/cart',Cartroutes)
 router.use('/upload',uploadroute)
 
 export default router
